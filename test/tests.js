15bd0ec50ff5c3fad02db35a9bd7ff240c6077b3
ecrmnn <danieleckermann@gmail.com>
2016-04-19 20:04:14 +0200

    added execa comment

--- test/tests.js
+++ test/tests.js
@@ -4,6 +4,18 @@ const chai = require('chai');
 const expect = require('chai').expect;
 
 describe('Module Boilerplate Test Suite', function () {
+//  let result;
+//
+//  before(function (done) {
+//    execa.shell('node ./src/index.js --flag').then(response => {
+//      result = response.stdout;
+//      done();
+//    }).catch(error => {
+//      console.log(error);
+//      done();
+//    });
+//  });
+
   it('should be awesome', function () {
     const awesome = 'awesome';
     expect(awesome).to.equal('awesome');
